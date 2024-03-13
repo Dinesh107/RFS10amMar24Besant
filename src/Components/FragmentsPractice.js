dc6f208a385bd2c378a73ea26cbeb6dbc77019d6
Dinesh107 <dineshjr107@gmail.com>
2024-03-13 11:51:36 +0530

    FourthCommit

---
+++ src/Components/FragmentsPractice.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import TableRowForFragmentsPractice from './TableRowForFragmentsPractice'
+
+class FragmentsPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Students, This is Fragment Practice</h1>
+        <TableRowForFragmentsPractice/>
+      </div>
+    )
+  }
+}
+
+export default FragmentsPractice
---
+++ src/Components/TableColumnFragmentsPractice.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class TableColumnFragmentsPractice extends Component {
+  render() {
+    return (
+      <>
+        <td>Student1</td>
+        <td>Student2</td>
+      </>
+    )
+  }
+}
+
+export default TableColumnFragmentsPractice
